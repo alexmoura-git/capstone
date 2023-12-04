e3ceb12cd3e7c7728a1bc6ef30ce61e5a9d7e2d0
Alex Moura <alexmoura@alexmoura.com>
2023-12-04 12:31:28 -0500

    new homepage

--- app/Home.py
+++ app/Home.py
@@ -1,38 +1,27 @@
 import streamlit as st
-import pandas as pd 
 
-st.write("Hello, World more!")
+# Set page title and icon
+st.set_page_config(page_title="InsideAirbnb Data Analysis", page_icon="🏡")
 
-df = pd.read_csv('app/trimmed_listings.csv')
-df
+# Page content
+st.title("Unlocking Insights from InsideAirbnb Data")
+st.write("Welcome to our capstone academic project, where we delve deep into the treasure trove of data from InsideAirbnb, aligning with the mission of transparency and understanding within the vacation rental industry.")
 
+# Add mission alignment section
+st.header("Our Mission Alignment:")
+st.write("InsideAirbnb is committed to providing data transparency and facilitating informed decisions in the Airbnb marketplace. Our project is rooted in this mission and aims to contribute to InsideAirbnb's goal by offering the following analyses:")
 
-def page_home():
-    st.title("Home Page")
-    st.write("Welcome to the Home Page!")
+# Add links to analysis pages
+st.subheader("Explore Our Analyses:")
+ml_price_prediction_link = "[ML Price Prediction Analysis](link-to-streamlit-page)"
+entire_vs_partial_index_link = "[Entire House vs. Partial House Index](link-to-streamlit-page)"
+choropleth_map_link = "[Choropleth of Airbnb vs. Available Housing](link-to-streamlit-page)"
+st.markdown(f"- {ml_price_prediction_link}")
+st.markdown(f"- {entire_vs_partial_index_link}")
+st.markdown(f"- {choropleth_map_link}")
 
-# Page 2
-def page_about():
-    st.title("About Page")
-    st.write("This is the About Page!")
+# Add project image(s)
+st.image("project_image.jpg", caption="InsideAirbnb Data Analysis")
 
-# Page 3
-def page_contact():
-    st.title("Contact Page")
-    st.write("You can reach us at contact@example.com.")
-    
-    
-    
-def main():
-    st.sidebar.title("Navigation")
-    page = st.sidebar.radio("Go to:", ["Home", "About", "Contact"])
-
-    if page == "Home":
-        page_home()
-    elif page == "About":
-        page_about()
-    elif page == "Contact":
-        page_contact()
-
-if __name__ == "__main__":
-    main()
+# Footer
+st.write("Join us on this journey to uncover valuable insights, contribute to data transparency, and foster an informed Airbnb community.")
