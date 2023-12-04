eadf45c2bc33e9b286dfa4971753b53549e51877
Alex Moura <alexmoura@alexmoura.com>
2023-12-04 12:38:35 -0500

    new home page

--- app/Home.py
+++ app/Home.py
@@ -1,27 +1,33 @@
 import streamlit as st
 
 # Set page title and icon
-st.set_page_config(page_title="InsideAirbnb Data Analysis", page_icon="🏡")
+st.set_page_config(page_title="Unlocking Insights from InsideAirbnb Data", page_icon="🏡")
 
-# Page content
-st.title("Unlocking Insights from InsideAirbnb Data")
-st.write("Welcome to our capstone academic project, where we delve deep into the treasure trove of data from InsideAirbnb, aligning with the mission of transparency and understanding within the vacation rental industry.")
+# Project Description
+st.markdown("## Project Description:")
+st.write("Welcome to our capstone academic project, where we delve deep into the treasure trove of data from InsideAirbnb, aligning with the mission of transparency and understanding within the vacation rental industry. Through meticulous analysis and cutting-edge machine learning techniques, we've embarked on a journey to empower both hosts and travelers with valuable insights.")
 
-# Add mission alignment section
-st.header("Our Mission Alignment:")
+# Mission Alignment
+st.markdown("### Our Mission Alignment:")
 st.write("InsideAirbnb is committed to providing data transparency and facilitating informed decisions in the Airbnb marketplace. Our project is rooted in this mission and aims to contribute to InsideAirbnb's goal by offering the following analyses:")
 
-# Add links to analysis pages
-st.subheader("Explore Our Analyses:")
-ml_price_prediction_link = "[ML Price Prediction Analysis](link-to-streamlit-page)"
-entire_vs_partial_index_link = "[Entire House vs. Partial House Index](link-to-streamlit-page)"
-choropleth_map_link = "[Choropleth of Airbnb vs. Available Housing](link-to-streamlit-page)"
-st.markdown(f"- {ml_price_prediction_link}")
-st.markdown(f"- {entire_vs_partial_index_link}")
-st.markdown(f"- {choropleth_map_link}")
+# Analysis 1: ML Price Prediction
+st.markdown("1. **ML Price Prediction Analysis:**")
+st.write("We've developed a machine learning model to predict Airbnb prices accurately. Our analysis helps hosts understand the factors influencing their pricing strategy, ensuring competitive rates and maximizing revenue.")
+ml_price_prediction_link = "[Check out our interactive ML Price Prediction page here.](link-to-streamlit-page)"
+st.markdown(ml_price_prediction_link)
 
-# Add project image(s)
-st.image("project_image.jpg", caption="InsideAirbnb Data Analysis")
+# Analysis 2: Entire House vs. Partial House Index
+st.markdown("2. **Entire House vs. Partial House Index:**")
+st.write("To assist travelers in finding their ideal accommodations, we've created an index that compares entire houses to partial houses based on various parameters like price, amenities, and location.")
+entire_vs_partial_index_link = "[Discover the insights on our Entire House vs. Partial House Index here.](link-to-streamlit-page)"
+st.markdown(entire_vs_partial_index_link)
 
-# Footer
+# Analysis 3: Choropleth of Airbnb vs. Available Housing
+st.markdown("3. **Choropleth of Airbnb vs. Available Housing:**")
+st.write("Explore a visual representation of Airbnb listings compared to available housing in different regions. Our choropleth maps provide an in-depth perspective on the distribution of Airbnb properties and their impact on the local housing market.")
+choropleth_map_link = "[Explore the maps here.](link-to-streamlit-page)"
+st.markdown(choropleth_map_link)
+
+# Conclusion and Call to Action
 st.write("Join us on this journey to uncover valuable insights, contribute to data transparency, and foster an informed Airbnb community.")
