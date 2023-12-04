e91ea404f3dac5d9ac3591c4a7535b5ad452511d
Alex Moura <alexmoura@alexmoura.com>
2023-12-03 23:48:37 -0500

    Initial commit3

---
+++ config.py
@@ -0,0 +1,42 @@
+url = 'http://insideairbnb.com/get-the-data'
+
+list_of_files = ['listings.csv.gz']
+
+list_of_cities =  [
+    'asheville',
+    'austin',
+    'boston',
+    'broward-county',
+    'cambridge',
+    'chicago',
+    'clark-county',
+    'columbus',
+    'dallas',
+    'denver',
+    'hawaii',
+    'jersey-city',
+    'los-angeles',
+    'nashville',
+    'new-orleans',
+    'new-york-city',
+    'oakland',
+    'pacific-grove',
+    'palm-springs',
+    'philadelphia',
+    'portland',
+    'raleigh',
+    'salem',
+    'san-clara-county',
+    'san-diego',
+    'san-francisco',
+    'san-mateo-county',
+    'santa-cruz-county',
+    'santa-monica',
+    'seattle',
+    'twin-cities',
+    'washington-dc'
+]
+
+
+high_cut_off = 2000
+low_cut_off = 30
