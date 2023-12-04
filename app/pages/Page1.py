cd9852f0400b1dda7df58ce5e7de81cee3794af1
Alex Moura <alexmoura@alexmoura.com>
2023-12-04 00:19:45 -0500

    Adding App FIles3

---
+++ app/pages/Page1.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+st.write("Hello, World!")
+
+
+def page_home():
+    st.title("Home Page")
+    st.write("Welcome to the Home Page!")
+
+# Page 2
+def page_about():
+    st.title("About Page")
+    st.write("This is the About Page!")
+
+# Page 3
+def page_contact():
+    st.title("Contact Page")
+    st.write("You can reach us at contact@example.com.")
+    
+    
+    
+def main():
+    st.sidebar.title("Navigation")
+    page = st.sidebar.radio("Go to:", ["Home", "About", "Contact"])
+
+    if page == "Home":
+        page_home()
+    elif page == "About":
+        page_about()
+    elif page == "Contact":
+        page_contact()
+
+if __name__ == "__main__":
+    main()
