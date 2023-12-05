e6697233a4947f9acf7e7c163d0109c15e00f0bd
Alex Moura <alexmoura@alexmoura.com>
2023-12-04 22:10:05 -0500

    ki

--- app/Home.py
+++ app/Home.py
@@ -6,7 +6,7 @@ import streamlit as st
 # Set page title and icon
 st.set_page_config(page_title="Unlocking Insights from InsideAirbnb Data", page_icon="🏡")
 
-st.image("banner.png", caption="InsideAirbnb Data Analysis")
+
 
 # Project Description
 st.markdown("## Project Description:")
@@ -36,3 +36,5 @@ st.markdown(choropleth_map_link)
 
 # Conclusion and Call to Action
 st.write("Join us on this journey to uncover valuable insights, contribute to data transparency, and foster an informed Airbnb community.")
+
+st.image("app/banner.png", caption="InsideAirbnb Data Analysis")
