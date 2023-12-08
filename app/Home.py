f6a2f82e921b6c23e88e5f7cb3b92f13fe998539
Alex Moura <alexmoura@alexmoura.com>
2023-12-08 00:23:09 -0500

    erere

--- app/Home.py
+++ app/Home.py
@@ -4,7 +4,7 @@ import streamlit as st
 # Set page title and icon
 st.set_page_config(page_title="Impact of Airbnb on housing affordability", page_icon="🏡")
 
-st.image("app/banner.png", width=700)
+st.image("app/banner.png", width=800)
 
 
 
