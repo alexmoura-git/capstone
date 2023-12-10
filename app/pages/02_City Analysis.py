bc21bb8b878b08ceea35c969ce9b07805a3606bf
Alex Moura <alexmoura@alexmoura.com>
2023-12-09 21:51:45 -0500

    edits

--- app/pages/02_City Analysis.py
+++ app/pages/02_City Analysis.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 
+
+# Set page title and icon
+st.set_page_config(page_title="Impact of Airbnb on housing affordability", page_icon="🏡")
+
+st.image("app/banner.png", width=700)
+
 st.write("Hello, World!")
 
 
--- app/pages/04_Report.py
+++ app/pages/04_Report.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+# Set page title and icon
+st.set_page_config(page_title="Impact of Airbnb on housing affordability", page_icon="🏡")
+
+st.image("app/banner.png", width=700)
+
 st.write("Hello, World!")
 
 
