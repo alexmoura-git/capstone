65348405959329229a84fdcfdfe58ef6eea6079c
Alex Moura <alexmoura@alexmoura.com>
2023-12-10 11:23:03 -0500

    huhuh

--- app/pages/01_Affordability Predictor.py
+++ app/pages/01_Affordability Predictor.py
@@ -18,7 +18,7 @@ def one_hot_encode(df, column_name):
 
 # # Function to get neighborhoods 
 def get_neighborhoods(city):
-    return data[data['city'] == city]['neighbourhood_cleansed'].value_counts().head().index()
+    return data[data['city'] == city]['neighbourhood_cleansed'].unique()
 
 
 # # Function to get neighborhoods 
