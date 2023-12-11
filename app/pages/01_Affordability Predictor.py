bc7e9a49bb90a3f453a5bb1637ba5487b27c04ad
Alex Moura <alexmoura@alexmoura.com>
2023-12-11 06:00:03 -0500

    new with grapgh

--- app/pages/01_Affordability Predictor.py
+++ app/pages/01_Affordability Predictor.py
@@ -6,6 +6,83 @@ import numpy as np
 
 st.image("app/banner.png", width=700)
 
+
+import altair as alt
+import pandas as pd
+import numpy as np
+
+## chart ----------------
+def affordability_pressure_chart(predicted_rent, median_rent, max_val):
+    
+    max_val=max(predicted_rent, median_rent, max_val)
+
+    # Create Data Frame with Points
+    data = pd.DataFrame({
+        'Predicted AirbnB Income': [predicted_rent],
+        'Neigboorhood Median Rent': [median_rent]
+    })
+
+    # Base chart 
+    points = alt.Chart(data).mark_text(
+        text='🏠', 
+        fontSize=30,
+        align='center'
+    ).encode(
+        x='Predicted AirbnB Income:Q',
+        y='Neigboorhood Median Rent:Q'
+    )
+
+
+    # Create a DataFrame for the 45-degree line
+    line_data = pd.DataFrame({'x': np.linspace(0, max_val, 100)})
+    line_data['y'] = line_data['x']
+
+    # Create the area chart
+    area_chart = alt.Chart(line_data).mark_area(
+        line={'color':'darkred'},
+        color=alt.Gradient(
+            gradient='linear',
+            stops=[alt.GradientStop(color='white', offset=0),
+                   alt.GradientStop(color='red', offset=1)],
+            x1=0,
+            x2=1,
+            y1=0,
+            y2=1
+        )
+    ).encode(
+    x=alt.X('x', axis=alt.Axis(title='Predicted AirbnB Income')),  # Removing x-axis title
+    y=alt.Y('y', axis=alt.Axis(title='Neigboorhood Median Rent'))  # Setting y-axis title
+)
+
+
+    text = alt.Chart(pd.DataFrame({'x': [max_val * 0.95], 'y': [max_val * 0.05], 'text': ['Affordability Pressure Region']})).mark_text(
+        align='right',
+        baseline='bottom',
+        fontSize=12,
+        color='white'
+    ).encode(
+        x='x:Q',
+        y='y:Q',
+        text='text:N'
+    )
+
+
+    return_chart = (area_chart + points + text).configure_axis(
+        grid=False
+    ).properties(
+    width=250,
+    height=250
+)
+
+
+
+    return return_chart 
+
+## chart ----------------
+
+
+
+
 # Load trained model
 model = joblib.load('lightgbm_prod_model.pkl')
 
@@ -117,6 +194,11 @@ if st.sidebar.button('PREDICT'):
     airbnb_fee = 0.16
     occupancy_rate = 0.5
     medium_neiboorhood_rental = 200
+    predicted_monthly_income = {prediction[0] * days_in_month * occupancy_rate * (1 - airbnb_fee)
+
+    chart = affordability_pressure_chart(predicted_monthly_income , 2000 5000)
+
+    st.altair_chart(chart, use_container_width=True)
 
     # Markdown table
     markdown_table =f"""
@@ -130,7 +212,7 @@ if st.sidebar.button('PREDICT'):
     | Number of days in a month            | {days_in_month} |
     | Airbnb service fee percentage        | {airbnb_fee}|
     | Occupancy rate (assumed) | {occupancy_rate}|
-    | **Net Predicted Monthly Rental Income** |**${prediction[0] * days_in_month * occupancy_rate * (1 - airbnb_fee):,.2f}**|
+    | **Net Predicted Monthly Rental Income** |**${predicted_monthly_income :,.2f}**|
 
     
     ##### FORMULA:
