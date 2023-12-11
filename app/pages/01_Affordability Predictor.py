c72465b66ab7d854102f4eea56129fea7a2e9e1b
Alex Moura <alexmoura@alexmoura.com>
2023-12-11 10:12:58 -0500

    ddf

--- app/pages/01_Affordability Predictor.py
+++ app/pages/01_Affordability Predictor.py
@@ -223,7 +223,7 @@ if st.sidebar.button('PREDICT'):
     st.altair_chart(chart, use_container_width=True)
 
     # Markdown table
-    markdown_table =f"""
+    markdown_table =f"""------
 
     **Affordability Methodology and Calculation:**
     1. Based on the predicted daily rental we canclculate the net predicted monthly rental as per table below:
