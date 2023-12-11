a2384f06e020f9fc0a63dee8db50330d7b69f941
Alex Moura <alexmoura@alexmoura.com>
2023-12-11 05:52:52 -0500

    commit

--- app/pages/04_Report.py
+++ app/pages/04_Report.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+from page_func import * 
 
 
 # Set page title and icon
@@ -7,5 +10,9 @@ st.set_page_config(page_title="Impact of Airbnb on housing affordability", page_
 st.image("app/banner.png", width=700)
 
 st.write("Hello, World!")
+
+chart = affordability_pressure_chart(2000, 344, 500)
+
+st.altair_chart(chart, use_container_width=True)
 
 
